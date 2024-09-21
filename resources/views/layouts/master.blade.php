b19351fb99094a3e2e5610f12b47307a8efa70f0
劉秉樺 <alen920617@gmail.com>
2024-09-21 12:33:23 +0800

    首頁畫面建置

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,8 +16,6 @@
 {{--        @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Ma+Shan+Zheng&display=swap" rel="stylesheet">
@@ -42,6 +40,10 @@
         </style>
         <!-- Google Fonts字型 -->
         <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/IcoFont/1.0.0/icofont.min.css">
+
     </head>
     <body>
         @include('layouts.partials.navigation')
@@ -61,6 +63,67 @@
             a {
                 text-decoration:none;
             }
+        </style>
+
+        <style>
+            .button-name {
+                align-items: center;
+                appearance: none;
+                background-color: #fcfcfd;
+                border-radius: 4px;
+                border-width: 0;
+                box-shadow:
+                    rgba(45, 35, 66, 0.2) 0 2px 4px,
+                    rgba(45, 35, 66, 0.15) 0 7px 13px -3px,
+                    #d6d6e7 0 -3px 0 inset;
+                box-sizing: border-box;
+                color: #36395a;
+                cursor: pointer;
+                display: flex;
+                flex-direction: column;
+                height: 150px;
+                width: 150px;
+                justify-content: center;
+                padding: 16px;
+                position: relative;
+                text-align: center;
+                text-decoration: none;
+                transition:
+                    box-shadow 0.15s,
+                    transform 0.15s;
+                user-select: none;
+                -webkit-user-select: none;
+                touch-action: manipulation;
+                white-space: nowrap;
+                will-change: box-shadow, transform;
+                font-size: 18px;
+            }
+
+            .button-name i {
+                margin-bottom: 10px;
+            }
+
+            .button-name:focus {
+                box-shadow:
+                    #d6d6e7 0 0 0 1.5px inset,
+                    rgba(45, 35, 66, 0.4) 0 2px 4px,
+                    rgba(45, 35, 66, 0.3) 0 7px 13px -3px,
+                    #d6d6e7 0 -3px 0 inset;
+            }
+
+            .button-name:hover {
+                box-shadow:
+                    rgba(45, 35, 66, 0.3) 0 4px 8px,
+                    rgba(45, 35, 66, 0.2) 0 7px 13px -3px,
+                    #d6d6e7 0 -3px 0 inset;
+                transform: translateY(-2px);
+            }
+
+            .button-name:active {
+                box-shadow: #d6d6e7 0 3px 7px inset;
+                transform: translateY(2px);
+            }
+
         </style>
     </body>
 </html>
