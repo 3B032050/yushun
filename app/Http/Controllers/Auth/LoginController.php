664648fb8e427ba0723620e3595d2a5afd8f4b97
黃文輝 <antory040512@gmail.com>
2025-04-25 10:53:19 +0800

    微調

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,10 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/users/index';
+    protected function authenticated($request, $user)
+    {
+        return redirect('/users/index');
+    }
 
     /**
      * Create a new controller instance.
