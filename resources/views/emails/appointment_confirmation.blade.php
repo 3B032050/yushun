acc9dfe260994474816537452538d876676ce148
黃文輝 <antory040512@gmail.com>
2025-08-14 16:41:22 +0800

    bug處理

--- resources/views/emails/appointment_confirmation.blade.php
+++ resources/views/emails/appointment_confirmation.blade.php
@@ -10,14 +10,18 @@
 <p>親愛的 {{ $user_name ?? '未知用戶' }}，</p>
 <p>師傅: {{ $master_name ?? '未知師傅' }}</p>
 <p>狀態:
-    @if(($status ?? 0) == 1)
+    @if(isset($status) && $status == 1)
         您的預約已成功安排。
-    @elseif(($status ?? 0) == 3)
+    @elseif(isset($status) && $status == 0)
+        您的預約正在等待確認，請耐心等候。
+    @elseif(isset($status) && $status == 3)
         很抱歉，您的預約未被通過，歡迎重新預約或洽詢客服。
     @else
         未知狀態
     @endif
 </p>
+
+
 
 
 <p>預約日期: {{ $service_date ?? '未指定日期' }}</p>
--- resources/views/users/personal_information/edit.blade.php
+++ resources/views/users/personal_information/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('users.layouts.master')
 
-@section('title','家居媒合服務平台')
+@section('title','豫順家居服務媒合平台')
 
 @section('content')
     <div class="content-wrapper">
